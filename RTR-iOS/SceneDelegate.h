02eda9fb74ba969099aab84f1184250aa30a66e8
SYSUcarey <944131226@qq.com>
2020-02-22 09:30:28 +0800

    Initial Commit

---
+++ RTR-iOS/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  RTR-iOS
+//
+//  Created by chenbb6 on 2020/2/22.
+//  Copyright © 2020 chenbb6. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ RTR-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RTR-iOS
+//
+//  Created by chenbb6 on 2020/2/22.
+//  Copyright © 2020 chenbb6. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
